de8273461879fab6baac992cea573222d3963a3d
REBA FATIMA FAWAD <rebaff88@gmail.com>
2026-05-02 22:39:40 +0500

    Enhancing and adding comments to the header file

--- Code/Vehicle.h
+++ Code/Vehicle.h
@@ -1,46 +1,37 @@
 #ifndef VEHICLE_H
-#define VEHICLE_H
-
+#define VEHICLE_H//format of the header file:
+//header file 
 // ============================================================
 //  Vehicle.h
 //  DEVELOPED BY: Reba Fatima Fawad (25L-2016) - Team Lead
 //  Responsibility: Vehicle entity + encapsulated data
 // ============================================================
-
-#include <string>
-#include <ctime> 
-
+#include <string>//library foir string 
+#include <ctime> //libraries used for tinme manipulation and for string handling 
 using namespace std;
-
-class Vehicle {
-private:
-    string vehicleNumber;
-    string vehicleType;     // Car, Bike, Truck
-    time_t entryTimestamp;
-    time_t exitTimestamp;
-
+class Vehicle {//here in am going to cretae the class of the vehicle 
+private://creating the data members as private 
+    string vehicleNumber;//the number; other essentials are:
+    string vehicleType;//what type of the vehicle 
+    time_t entryTimestamp;//entry time of the vehicle 
+    time_t exitTimestamp;//exit time of the vehicle 
 public:
-    Vehicle();
-    Vehicle(const string& vNum, const string& vType);
-
-    void setVehicleNumber(const string& vNum);
+    Vehicle();//defult constructor 
+    Vehicle(const string& vNum, const string& vType);//parametrized constructors
+    void setVehicleNumber(const string& vNum);//functions for different actions regarding the class respectively
     void setVehicleType(const string& vType);
     void setEntryTime(time_t t);
     void setExitTime(time_t t);
-
     string getVehicleNumber() const;
     string getVehicleType()   const;
     time_t getEntryTimestamp() const;
     time_t getExitTime()       const;
-
     string getEntryTimeStr()  const;
     string getExitTimeStr()   const;
     double getParkingDurationHours() const;
-
     void displayVehicleInfo() const;
-
 private:
     static string timeToStr(time_t t);
 };
-
+//here is the end of the header file and i have created the class of the vehicle with all the data members and functions which are required for the operations of the parking management system
 #endif // VEHICLE_H
