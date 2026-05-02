fcad09b37f239ea3a2a80cce23c53d5085b67286
abdulahhx <abdulahhx14@gmail.com>
2026-05-02 18:38:58 +0500

    load and save information in history.txt
    Added load and save functions for parking history

--- ParkingLot.cpp
+++ ParkingLot.cpp
@@ -31,3 +31,46 @@ void ParkingLot::initialize(int numSlots) {
 bool ParkingLot::isInitialized() const { 
     return totalSlots > 0;
 }
+//LOAD AND SAVE INFO IN HISTORY FILE
+// Format: username,vehicleNum,vehicleType,entry,exit,duration,fee,slot,lockedRate
+void ParkingLot::loadHistory() {
+    historyCount = 0;
+    ifstream fin(HISTORY_FILE.c_str());
+    if (!fin.is_open()) return;
+    string line;
+    while (getline(fin, line) && historyCount < MAX_HISTORY) {
+        if (line.empty()) continue;
+        stringstream ss(line);
+        ParkingRecord rec;
+        string dur, fee, slot, lockedRate;
+        getline(ss, rec.username, '|');
+        getline(ss, rec.vehicleNumber, '|');
+        getline(ss, rec.vehicleType, '|');
+        getline(ss, rec.entryTime, '|');
+        getline(ss, rec.exitTime, '|');
+        getline(ss, dur, '|');
+        getline(ss, fee, '|');
+        getline(ss, slot, '|');
+        getline(ss, lockedRate, '|');
+        
+        rec.duration = atof(dur.c_str());
+        rec.fee = atof(fee.c_str());
+        rec.slotNumber = atoi(slot.c_str());
+        if (lockedRate.empty() || lockedRate == "0") {
+            rec.lockedRate = (rec.duration > 0) ? (rec.fee / rec.duration) : rateValues[0];
+        }
+        else {
+            rec.lockedRate = atof(lockedRate.c_str());
+        }
+        history[historyCount++] = rec;
+    }
+    fin.close();
+}
+void ParkingLot::saveHistory() {
+    ofstream fout(HISTORY_FILE.c_str());
+    for (int i = 0; i < historyCount; i++) {
+        const ParkingRecord& rec = history[i];
+        fout << rec.username << "|" << rec.vehicleNumber << "|" << rec.vehicleType << "|" << rec.entryTime << "|" << rec.exitTime << "|"
+            << fixed << setprecision(2) << rec.duration << "|" << rec.fee << "|" << rec.slotNumber << "|"  << rec.lockedRate << endl;
+    }out.close();
+}
