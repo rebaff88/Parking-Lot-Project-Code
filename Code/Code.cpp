ff1663bf9d32f1bb1b907904c89ee53ea01fefe5
REBA FATIMA FAWAD <rebaff88@gmail.com>
2026-05-08 20:47:22 +0500

    adding Comments and enhancing data

--- Code/Code.cpp
+++ Code/Code.cpp
@@ -3,17 +3,27 @@
 //  DEVELOPED BY: Reba Fatima Fawad (25L-2016) - Team Lead
 // ============================================================
 #include "Vehicle.h"//here i have mentioned the header file of vehicle class
+//now thye contents of the header files acn be accessd using this  
 #include <iostream>//main library
 #include <iomanip>//format output, sets field for the output required 
+//iomap: formsting, may be used in future
 #include <sstream>//string stream library for string manipulation
+//string stream  manipulation is accesd using this library
 #include <cstring>//allows many functions which ican use for string functions and manipulation 
 using namespace std;
 Vehicle::Vehicle() : entryTimestamp(0), exitTimestamp(0) {}//here i am going to create a default consyructor
+//:: is the scope resolution operastoer, now this is used to tell
+//that this content is the data  mamber of the class which here ids the vehicle class
+//object created, from constructor is called
+//initailizing the time from Zero, as before the object which is a vehicle
+//thye initialk time nis setted as to be zero
+//constructor body is left empty as all,m the functions, i have performed in the initialization of the constructor itslef
 Vehicle::Vehicle(const string& vNum, const string& vType)//here i am going to create a parametrized constructor 
     : vehicleNumber(vNum), vehicleType(vType),
     entryTimestamp(0), exitTimestamp(0) {//values are initialized and assigned 
 }//moreover the constructors are left empty as I only wanted to assign the possible scenarios 
 //so that any type of objects can be handled 
+//now agfain using the format passing and initializiung the dsata members 
 void Vehicle::setVehicleNumber(const string& vNum) { vehicleNumber = vNum; }//notations used from the libraries 
 void Vehicle::setVehicleType(const string& vType) { vehicleType = vType; }
 void Vehicle::setEntryTime(time_t t) { entryTimestamp = t; }//setting the values 
@@ -23,12 +33,19 @@ string Vehicle::getVehicleNumber() const { return vehicleNumber; }//all the valu
 string Vehicle::getVehicleType()   const { return vehicleType; }
 time_t Vehicle::getEntryTimestamp()const { return entryTimestamp; }
 time_t Vehicle::getExitTime()      const { return exitTimestamp; }
+//again with these gatter functions the data menmbers of the object are returned
 //here I am going to create some static funtiond and operatioons 
-string Vehicle::timeToStr(time_t t) {
-    if (t == 0) return "N/A";
-    char buf[64];
-    struct tm* tm_info = localtime(&t);
-    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", tm_info);
+string Vehicle::timeToStr(time_t t) {//again i  am gloling to create a functioin wherre the time 9in seconds is attained
+    //is going toi get converted into string rather then the secobnds
+    if (t == 0) return "N/A";//hence 0 seconds, not available
+    char buf[64];//making a charcter array 
+    //initialized with the value of the character array as 64 is trhe
+    //character capacity of thye data type character itself
+   //from 0-63
+    struct tm* tm_info = localtime(&t);//using struct: to initialize multiple data content in one place
+    //for example time is enholding the : days, montthZ and hourZ
+    //tm is pointer , pointing to localtime(&t): which will convt the raw data in seconds to readsable data ie string
+    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", tm_info);//final formating to days-months etc
     return string(buf);
 }
 string Vehicle::getEntryTimeStr() const { return timeToStr(entryTimestamp); }
@@ -39,10 +56,13 @@ string Vehicle::getExitTimeStr()  const {
 double Vehicle::getParkingDurationHours() const {
     if (entryTimestamp == 0 || exitTimestamp == 0) return 0.0;
     double diff = difftime(exitTimestamp, entryTimestamp);
-    return diff / 3600.0;
+    return diff / 3600.0;//1 hour comprise 60 mins which inholds 60 seconds
+    //that is why 60 x 60 =b 3600 seconds per hour
 }
 //now the details of the vehicles:
 void Vehicle::displayVehicleInfo() const {
+    //the function from the header file which will demonxstratye trhe details comprising
+    //of the data members of the vehicle objects 
     cout << "  Vehicle No   : " << vehicleNumber << "\n";
     cout << "  Vehicle Type : " << vehicleType << "\n";
     cout << "  Entry Time   : " << getEntryTimeStr() << "\n";
--- Code/Vehicle.h
+++ Code/Vehicle.h
@@ -1,10 +1,13 @@
-#ifndef VEHICLE_H
+#ifndef VEHICLE_H//to include file if previouzsly it was not definhed
 #define VEHICLE_H//format of the header file:
 //header file 
+// as another file(.cpp)can also access the header file to
+// //invlove the contents of the file
+// /so for the compiler to avoiod errors we arew adding the to[p labels
 // ============================================================
 //  Vehicle.h
 //  DEVELOPED BY: Reba Fatima Fawad (25L-2016) - Team Lead
-//  Responsibility: Vehicle entity + encapsulated data
+//  Responsibility: Vehicle entity + encapsulated data; which are the parts of content covered in OOP this semester
 // ============================================================
 #include <string>//library foir string 
 #include <ctime> //libraries used for tinme manipulation and for string handling 
@@ -15,23 +18,34 @@ private://creating the data members as private
     string vehicleType;//what type of the vehicle 
     time_t entryTimestamp;//entry time of the vehicle 
     time_t exitTimestamp;//exit time of the vehicle 
+    //time_t is accesedd from the library of ctime 
+    //time_t itsekf is a data member whioch stores ther data in itself in the form of seconds
+    // //entry and exit will get stored in the form oif seconds
 public:
-    Vehicle();//defult constructor 
+    //for direct accessing the functions and the constructors of the object of trhe class
+    Vehicle();//defult constructor has been cereeated here for the objects which does not enhoolds any value
+    //or is not parametrized
     Vehicle(const string& vNum, const string& vType);//parametrized constructors
+    //for the parametrized constructors we are passing the  now this will work for theb  paasing of the 
+    //vehiclew number and the type of thje vehicle 
     void setVehicleNumber(const string& vNum);//functions for different actions regarding the class respectively
+    //the above function is the setter function of the object
     void setVehicleType(const string& vType);
     void setEntryTime(time_t t);
     void setExitTime(time_t t);
-    string getVehicleNumber() const;
+    //till here i ahve made the setters for the objects contenty
+    //here i am going to create the getter fuctions of the cointent
+    string getVehicleNumber() const;//using const not to modify the objecvt oitsle just peerfornming the function on the data memebers of the object
     string getVehicleType()   const;
-    time_t getEntryTimestamp() const;
+    time_t getEntryTimestamp() const;//getting the data in a readable form
     time_t getExitTime()       const;
     string getEntryTimeStr()  const;
     string getExitTimeStr()   const;
-    double getParkingDurationHours() const;
-    void displayVehicleInfo() const;
+    double getParkingDurationHours() const;//double for attaing the time for example 2.5 instread of 2 hours and 30 mins 
+    void displayVehicleInfo() const;//this function will display trhe contents(genefral)
 private:
-    static string timeToStr(time_t t);
+    static string timeToStr(time_t t);//this data member is static because the function timeToStr itself is as mere utility function
+    //this allows thye converstion of the time we have attained to string
 };
 //here is the end of the header file and i have created the class of the vehicle with all the data members and functions which are required for the operations of the parking management system
 #endif // VEHICLE_H
